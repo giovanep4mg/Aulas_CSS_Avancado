da876d409c918ba8e82774f84dff198a1148a606
giovani <115195600+giovanep4mg@users.noreply.github.com>
2023-08-04 07:25:55 -0300

    ataulizando aulas casa, erro

---
+++ site-base-styled-components/meuapp2/src/pages/sobre.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sobre = () => (
+    <div className="title">
+        <h4>Sobre</h4>
+        <p>
+            Mussum Ipsum, cacilds vidis  litro abertis
+            Manduma <b>pindureta</b> quium dia nois paga. 
+            Viva Forevis aptent taciti sociosqu ad litora torquent.   
+            In elementis mé pra quem é amistosis quis leo. 
+            Diuretics paradis num copo é motivis de denguis.  
+        </p>
+    </div>
+);
+
+export default Sobre;
